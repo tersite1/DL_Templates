f023f5b67b12830a339c2c078d51d470be0870fc
~/tersite1 <160453556+tersite1@users.noreply.github.com>
2025-01-27 19:36:25 +0900

    Create BASIC_NETWORK.py

---
+++ BASIC_NETWORK.py
@@ -0,0 +1,20 @@
+
+import torch
+
+from torch import nn
+
+from torchvision import *
+
+
+class MNP(nn.Module):
+  def __init__(self)
+    super().__init__()
+
+
+  def forward(self,x)
+
+    return x 
+
+
+model = MNP()
+
