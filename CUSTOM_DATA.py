421506af8f1e255fa23f18f31193dfbe2cd3f58f
~/tersite1 <160453556+tersite1@users.noreply.github.com>
2025-01-27 19:34:37 +0900

    Create CUSTOM_DATA.py

---
+++ CUSTOM_DATA.py
@@ -0,0 +1,20 @@
+class CUstom_Dataset(torch.utils.data.Dataset): ##커스텀 데이타셋 불러오는 폼
+
+
+    def __init__(self, X, Y, transform=None):  #transform : augmentation x:입력 y:정답
+
+        self.X = X
+        self.Y = Y
+        self.transform = transform
+
+    def __len__(self):
+
+        return self.X.shape[0] #개체행열 중 개
+
+    def __getitem__(self,idx):   #인덱스
+        self.X[idx]
+        if self.transform is not None:
+            x = self.transform(x)
+        y = self.Y[idx]
+
+        return x,y
